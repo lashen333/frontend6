8c6c83f00575c312d06e27e7aa4de908442dcff7
lashen333 <lashen.aenigm3labs@gmail.com>
2025-07-24 22:45:43 +0530

    New update analysis 2

--- src/types/VariantPerformance.types.ts
+++ src/types/VariantPerformance.types.ts
@@ -3,6 +3,7 @@
 export interface VariantPerformance {
   heading?: string;        // <-- add this if missing
   subtitle?: string;
+  title?: string;          // <-- add this if missing
   ctaText?: string;
   variantId: string;
   totalViews?: number;
